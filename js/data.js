5f9edea967cf62b211761d01f721714e780265a8
Doko <44224863+doko972@users.noreply.github.com>
2024-05-14 11:48:26 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,116 @@
 
+// Array Data JSON
+const itemsData = [];
+
+// Get items in JSON
+function loadItems() {
+    fetch('json/product.json')
+        .then(response => response.json())
+        .then(data => {
+            itemsData.push(...data); // Add data to itemsData
+            const container = document.getElementById('item-container');
+            itemsData.forEach(item => {
+                const article = document.createElement('div');
+                article.classList.add('item');
+
+                const name = document.createElement('div');
+                name.classList.add('name');
+                name.textContent = item.name;
+
+                const price = document.createElement('div');
+                price.classList.add('price');
+                price.textContent = item.price + " po";
+
+                const img = document.createElement('img');
+                img.src = item.imageUrl;
+                img.alt = item.altTxt;
+
+                const addToCartButton = document.createElement('button');
+                addToCartButton.textContent = "Ajouter au panier";
+                addToCartButton.addEventListener('click', function () {
+                    addPanier(item);
+                });
+
+                article.appendChild(name);
+                article.appendChild(price);
+                article.appendChild(img);
+                article.appendChild(addToCartButton);
+
+                container.appendChild(article);
+            });
+        })
+        .catch(error => console.error('Erreur lors du chargement des articles :', error));
+}
+
+// Add quantity to cart
+function addPanier(article) {
+    const cartList = document.getElementById('cart-list');
+
+    // Show items in cart
+    const existingItem = cartList.querySelector(`[data-id="${article._id}"]`);
+    if (existingItem) {
+        const quantityDisplay = existingItem.querySelector('.quantity');
+        const quantity = parseInt(quantityDisplay.textContent);
+        const availableQuantity = parseInt(article.quantité);
+        if (quantity < availableQuantity) {
+            quantityDisplay.textContent = quantity + 1;
+            UpdateTotalCart();
+        } else {
+            alert("La quantité disponible pour cet article est épuisée.");
+        }
+    } else {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-id', article._id);
+
+        const itemName = document.createElement('span');
+        itemName.textContent = article.name + " - " + article.price + " po";
+
+        const decreaseButton = document.createElement('button');
+        decreaseButton.textContent = "-";
+        decreaseButton.addEventListener('click', function () {
+            decrease(listItem, article.price);
+        });
+
+        const quantityDisplay = document.createElement('span');
+        quantityDisplay.classList.add('quantity');
+        quantityDisplay.textContent = "1"; // Init to 1
+
+        const increaseButton = document.createElement('button');
+        increaseButton.textContent = "+";
+        increaseButton.addEventListener('click', function () {
+            increaseQuantity(listItem, article.quantité, article.price);
+        });
+
+        listItem.appendChild(decreaseButton);
+        listItem.appendChild(quantityDisplay);
+        listItem.appendChild(increaseButton);
+        listItem.appendChild(itemName);
+        cartList.appendChild(listItem);
+
+        UpdateTotalCart(); // Total Cart
+    }
+}
+
+// Dicrease quantity in panier
+function decrease(listItem, price) {
+    const quantityDisplay = listItem.querySelector('.quantity');
+    const quantity = parseInt(quantityDisplay.textContent);
+    if (quantity > 1) { // Not quantity negative
+        quantityDisplay.textContent = quantity - 1;
+        UpdateTotalCart();
+    }
+}
+
+// Add quantity in panier
+function increaseQuantity(listItem, availableQuantity, price) {
+    const quantityDisplay = listItem.querySelector('.quantity');
+    const quantity = parseInt(quantityDisplay.textContent);
+    if (quantity < availableQuantity) {
+        quantityDisplay.textContent = quantity + 1;
+        UpdateTotalCart();
+    } else {
+        alert("La quantité disponible pour cet article est épuisée.");
+    }
+}
+
+window.onload = loadItems;
